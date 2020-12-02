479ff9272e2de7ed24abe4394a7fed345886bd97
Miriam Gurrieri <mion93@hotmail.it>
2020-12-02 17:56:41 +0000

    adding login functionality

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,16 @@ $(document).ready(function(){
    $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+
+
+
+
+
+ const slider = document.querySelector('.slider');
+    M.Slider.init(slider, {
+        indicators: false,
+        height: 500,
+        transition: 500,
+        interval: 6000
+      });
